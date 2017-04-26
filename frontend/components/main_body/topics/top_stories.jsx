85c49f0609db0aa6efd081a093a63a91785ce46c
James Stack <james.a.stack@gmail.com>
2017-04-26 11:58:50 -0700

    Add styling to topic pages

--- frontend/components/main_body/topics/top_stories.jsx
+++ frontend/components/main_body/topics/top_stories.jsx
@@ -2,6 +2,15 @@ import React from 'react';
 import fetchTopicStories from '../../../util/topics_api_util';
 import TopicItem from './topic_item';
 
+var topics = {
+  1: "Top Stories",
+  2: "Editor's Picks",
+  3: "Technology",
+  4: "Humans",
+  5: "Culture",
+  6: "Business"
+};
+
 class TopicFeed extends React.Component {
   constructor(props){
     super(props);
@@ -39,8 +48,10 @@ class TopicFeed extends React.Component {
   }
 
   render(){
+    const topicId = this.props.params.id;
     return(
       <div className="stories-index">
+        <h2 id="name">{topics[topicId]}</h2>
         {this.renderTopicItem()}
       </div>
     );
